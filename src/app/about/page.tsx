9871e73229b73a1389413333fe77b6b427d62345
rixcx <adts0415.ovo@gmail.com>
2024-04-21 16:55:30 +1000

    [ about ] test for build

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,8 +5,8 @@ import * as global from '../common/global'
 
 export default function About() {
   return (
-    <main css={test}>
-        <h1 css={[global.container,taitoru]}>About</h1>
+    <main>
+        <h1 css={global.container}>About</h1>
 
 
     </main>
@@ -14,19 +14,3 @@ export default function About() {
 }
 
 /*---------- css ----------*/
-export const test = css`
-  margin-top: 100px;
-  
-  ${global.sp} {
-    margin-top: 60px;
-  }
-`;
-export const taitoru = css`
-  font-size: var(--font-01);
-  font-weight: bold;
-  margin-bottom: 100px;
-  
-  ${global.sp} {
-    margin-bottom: 40px;
-  }
-`
