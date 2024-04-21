8e47c2ccc87d7a08f8093e07d536ac77c5b1e5e8
rixcx <adts0415.ovo@gmail.com>
2024-04-21 17:10:46 +1000

    [ about ] test for build

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import * as global from '../common/global'
+
+export default function About() {
+  return (
+    <main>
+        <h1>About</h1>
+    </main>
+  )
+}
+
+/*---------- css ----------*/
