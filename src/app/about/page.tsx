1212ecd4e6e02bc8e55a0b3e2ab6fa7976a007f1
rixcx <adts0415.ovo@gmail.com>
2024-04-21 17:15:09 +1000

    [ about ] test for build

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,10 +5,14 @@ import * as global from '../common/global'
 
 export default function About() {
   return (
-    <main>
+    <main css={test}>
         <h1>About</h1>
     </main>
   )
 }
 
 /*---------- css ----------*/
+export const test = css`
+  margin-top: 100px;
+
+`;
